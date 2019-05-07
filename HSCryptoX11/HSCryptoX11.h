eea84dba29964889b4e16b7f277e078c9797ea1b
Ermat <ealymbaev@gmail.com>
2019-05-07 12:01:20 +0600

    Fix warnings related to umbrella header: include headers to module map (#1)

--- HSCryptoX11/HSCryptoX11.h
+++ HSCryptoX11/HSCryptoX11.h
@@ -8,4 +8,16 @@ FOUNDATION_EXPORT const unsigned char HSCryptoKitVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <HSCryptoKit/PublicHeader.h>
 
-
+#import "BigIntTypes.h"
+#import "Blake.h"
+#import "Bmw.h"
+#import "CubeHash.h"
+#import "Groestl.h"
+#import "Echo.h"
+#import "Jh.h"
+#import "Keccak.h"
+#import "Luffa.h"
+#import "Shavite.h"
+#import "Simd.h"
+#import "Skein.h"
+#import "aes_helper.h"
