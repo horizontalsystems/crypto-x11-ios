e4a6741513553ee56183b9902d05e6662615ff2a
Anton <anton.stavnichiy@gmail.com>
2019-05-23 15:12:57 +0600

    Remove unused modulemap

--- HSCryptoX11/HSCryptoX11.h
+++ HSCryptoX11/HSCryptoX11.h
@@ -3,16 +3,3 @@
 FOUNDATION_EXPORT double HSCryptoX11VersionNumber;
 FOUNDATION_EXPORT const unsigned char HSCryptoX11VersionString[];
 
-#import "BigIntTypes.h"
-#import "Blake.h"
-#import "Bmw.h"
-#import "CubeHash.h"
-#import "Groestl.h"
-#import "Echo.h"
-#import "Jh.h"
-#import "Keccak.h"
-#import "Luffa.h"
-#import "Shavite.h"
-#import "Simd.h"
-#import "Skein.h"
-#import "aes_helper.h"
